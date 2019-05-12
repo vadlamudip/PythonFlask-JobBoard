dad746b2787a59af911b67373c9c179ca6ce4a54
Pandu Ranga Vadlamudi <pavadlam@microsoft.com>
2019-05-12 10:20:00 +0530

    SQL integration

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+import sqllite3
+from flask import Flask, render_template, g
 
+PATH = 'db/jobs.sqllite'
 app = Flask(__name__)
+
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqllite3.connect(PATH)
+    connection.row_factory = sqllite3.row
+    return connection
+
+def execute_sql(sql, values=(), commit = True, single = True):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+                    
+@app.teardown_appcontext                    
+def close_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
