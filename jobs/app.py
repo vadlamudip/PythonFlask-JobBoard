0c2fd53ecf9261f6eff0f82b3b1bfa330add5d7f
Pandu Ranga Vadlamudi <pavadlam@microsoft.com>
2019-05-12 10:42:36 +0530

    fixes

--- jobs/app.py
+++ jobs/app.py
@@ -19,7 +19,9 @@ def execute_sql(sql, values=(), commit = False, single = False):
         results = connection.commit
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-                    
+    cursor.close()
+    return results
+
 @app.teardown_appcontext                    
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
